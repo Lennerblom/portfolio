5977ec3ac400ccef8ef660f64ac0db56e4246c0f
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-04-19 20:50:25 -0700

    intro paragraph updates, style updates

--- portfolio/src/home.js
+++ portfolio/src/home.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment }  from 'react';
 import './styles/practice.scss';
-import Rotator from './rotator';
+//import Rotator from './rotator';
 import ProjectCard from './projectCard';
 
 export default class Home extends Component {
@@ -14,8 +14,9 @@ export default class Home extends Component {
                   <li className="nav"><a href="https://gist.github.com/Lennerblom/bd99389a3f6201a19e7caf741a26f0c8">resume</a></li>
                 </ul>
               </nav>
-              <div className='face-container'>
-                <img src={require('./assets/MyFace.jpg') } alt="my face" width="300px"/>
+              <div className='intro-container'>
+                <img className='face-image' src={require('./assets/MyFace.jpg') } alt="my face" width="300px"/>
+                <div className='intro-paragraph'>Hi, my name is Michael Lennerblom and I am a software developer in the greater Seattle area. I graduated from a program where I learned full stack development, and I’m excited to continue learning more.  Please click on a project below to view my work. </div>
               </div>
                 <div className="container projects">
                 <ProjectCard/>
